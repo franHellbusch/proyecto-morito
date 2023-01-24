e1b6aa3155cff221730712b270837e3e17b5009e
FranciscoHellbusch <fghellbusch@gmail.com>
2023-01-24 12:34:16 -0300

    formulario terminado

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-(function() {
+(function () {
     emailjs.init('JOnUIgvKxSKzm0vzz')
 })()
 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
 
         const checkbox = document.getElementById('newsLetterCheck');
         this.news_letter.value = checkbox.checked ? 'Quiero suscribirme a nueva informacion en este email' : 'No quiero recibir noticias'
 
         emailjs.sendForm('service_n48939a', 'template_8yik7cp', this)
-            .then(function() {
+            .then(function () {
                 console.log('SUCCESS!');
-            }, function(error) {
+            }, function (error) {
                 console.log('FAILED...', error);
             });
     });
