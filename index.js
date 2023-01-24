d683645e5ed1a05cbd2c3b86362c5b3c9dab5235
FranciscoHellbusch <fghellbusch@gmail.com>
2023-01-24 11:21:18 -0300

    form terminado

---
+++ index.js
@@ -0,0 +1,19 @@
+(function() {
+    emailjs.init('JOnUIgvKxSKzm0vzz')
+})()
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const checkbox = document.getElementById('newsLetterCheck');
+        this.news_letter.value = checkbox.checked ? 'Quiero suscribirme a nueva informacion en este email' : 'No quiero recibir noticias'
+
+        emailjs.sendForm('service_n48939a', 'template_8yik7cp', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
